559952c8700776e820d03210d558cd7dbd548db8
Filip Parwa <f.parwa@delante.pl>
2024-04-17 21:13:41 +0200

    styling about us

--- template-frontpage.php
+++ template-frontpage.php
@@ -14,28 +14,24 @@
 	</section>
 
 	<section id="about-us" class="w-section about-us">
-		<div class="content-wrapper container">
+		<div class="container about_us">
 			<div class="about-us-header">
 				<h2>O nas</h2>
 			</div>
-			<div class="row about-us-content">
-				<div class="text-column col-lg-6 col-md-6 col-sm-12">
-					<div class="about-us-text">
-						<h3>Zabudowa meblowa dla inwestycji</h3> <!-- First section title -->
-						<div class="stripe"></div> <!-- Title stripe -->
+			<div class="row">
+				<div class="col-lg-6 col-md-6 col-sm-12 about_us__col">
+					<div class="">
+						<h3 class="about_us__heading">Zabudowa meblowa dla inwestycji</h3>
 						<p>Kompleksowo zajmujemy się produkcją mebli do powierzchni komercyjnych takich jak biurowce, kancelarie, sklepy, salony optyczne, banki, biura i fabryki. Od projektu po wykonanie.</p>
 					</div>
-					<div class="about-us-text">
-						<h3>Stawiamy na profesjonalizm</h3> <!-- Second section title -->
-						<div class="stripe"></div> <!-- Title stripe -->
+					<div class="">
+						<h3 class="about_us__heading">Stawiamy na profesjonalizm</h3>
 						<p>Jesteśmy zespołem fachowców, których łączy kilkunastoletnie doświadczenie, znajomość branży i pasja. Nie zdarzyło nam się dwa razy zrobić takiej samej realizacji - każde zlecenie wymaga indywidualnego podejścia i szukania innowacyjnych rozwiązań. Stawiamy na rzetelność wykonania i terminowość. </p>
 					</div>
 				</div>
-				<div class="about-us-video col-lg-6 col-md-6 col-sm-12">
-					<h3>Spacer 3D</h3> <!-- Video section title -->
-					<div class="stripe"></div> <!-- Title stripe -->
-					<!-- Replace the URL with the embed code provided by YouTube -->
-					<iframe src="https://www.youtube.com/embed/lVdYDLwM8HI?si=ePQYRT0bWxX5NUQp" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+				<div class="col-lg-6 col-md-6 col-sm-12 about_us__col">
+					<h3 class="about_us__heading">Spacer 3D</h3>
+					<iframe src="https://www.youtube.com/embed/lVdYDLwM8HI?si=ePQYRT0bWxX5NUQp" class="video" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 				</div>
 			</div>
 		</div>
