d123047bc7396eff81caa5a5952bf3e411cc5746
immersiveartkamil <immersiveartstudio@gmail.com>
2024-04-17 20:26:24 +0200

    bootstrap

--- template-frontpage.php
+++ template-frontpage.php
@@ -12,13 +12,14 @@
 			<div class="slide"></div>
 		</div>
 	</section>
-	<section class="container w-section about-us">
-		<div class="content-wrapper">
+
+	<section id="about-us" class="w-section about-us">
+		<div class="content-wrapper container">
 			<div class="about-us-header">
 				<h2>O nas</h2>
 			</div>
-			<div class="about-us-content">
-				<div class="text-column">
+			<div class="row about-us-content">
+				<div class="text-column col-lg-6 col-md-6 col-sm-12">
 					<div class="about-us-text">
 						<h3>Zabudowa meblowa dla inwestycji</h3> <!-- First section title -->
 						<div class="stripe"></div> <!-- Title stripe -->
@@ -30,7 +31,7 @@
 						<p>Jesteśmy zespołem fachowców, których łączy kilkunastoletnie doświadczenie, znajomość branży i pasja. Nie zdarzyło nam się dwa razy zrobić takiej samej realizacji - każde zlecenie wymaga indywidualnego podejścia i szukania innowacyjnych rozwiązań. Stawiamy na rzetelność wykonania i terminowość. </p>
 					</div>
 				</div>
-				<div class="about-us-video">
+				<div class="about-us-video col-lg-6 col-md-6 col-sm-12">
 					<h3>Spacer 3D</h3> <!-- Video section title -->
 					<div class="stripe"></div> <!-- Title stripe -->
 					<!-- Replace the URL with the embed code provided by YouTube -->
