2f02478957c99d6d98ffb3a0bd9c0eae5e2e7922
Filip Parwa <f.parwa@delante.pl>
2024-04-22 00:34:37 +0200

    fix

--- footer.php
+++ footer.php
@@ -40,19 +40,9 @@
             <div class="col-lg-5 col-md-6 col-sm-12">
                 <div class="form">
                     <h3>Co możemy dla Ciebie zrobić?</h3>
-                    <form action="/submit_contact_form" method="post">
-                        <div class="form-wrapper">
-                            <div class="form-group">
-                                <input type="text" id="name" name="name" class="form-control" placeholder="Twoje imię i nazwisko" required>
-                            </div>
-                            <div class="form-group">
-                                <input type="email" id="email" name="email" class="form-control" placeholder="Twój adres e-mail" required>
-                            </div>
-                            <div class="form-group">
-                                <input type="tel" id="phone" name="phone" class="form-control" placeholder="Twój numer telefonu" required>
-                            </div>
-                            <button type="submit" class="btn btn-primary">Wyślij</button>
-                        </div>
+                    <div class="footer-contact">
+                        <?php echo do_shortcode('[contact-form-7 id="121c6b6" title="Formularz stopka"]'); ?>
+                    </div>
                 </div>
             </div>
 
